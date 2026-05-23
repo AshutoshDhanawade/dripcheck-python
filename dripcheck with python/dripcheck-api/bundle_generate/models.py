e06c4c80ce2d5f38b12fb56f771d3042072f5972
unknown <payalgawade62@gmail.com>
2026-05-24 01:27:46 +0530

    Added new flow for existing users like what data should be shown to user

--- dripcheck with python/dripcheck-api/bundle_generate/models.py
+++ dripcheck with python/dripcheck-api/bundle_generate/models.py
@@ -21,6 +21,7 @@ class MerchantProduct(models.Model):
     aesthetic_tone = models.CharField(max_length=255, null=True, blank=True)
     image_url = models.URLField(max_length=1000, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    sales_count = models.IntegerField(default=0)
     added_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
